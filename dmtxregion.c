d8aaa1a841bf67dc8e713c63510a02eecb81a0b9
Vitaly Toktarov <70091281+toktarov84@users.noreply.github.com>
2023-11-24 10:54:35 +0400

    Update dmtxregion.c

--- dmtxregion.c
+++ dmtxregion.c
@@ -712,8 +712,8 @@ MatrixRegionFindSize(DmtxDecode *dec, DmtxRegion *reg)
    }
 
    /* Если ни один из размеров не обеспечил приемлемого контраста, то считайте, что все завершено */
-   if(bestSizeIdx == DmtxUndefined || bestContrast < 20)
-      return DmtxFail;
+   //if(bestSizeIdx == DmtxUndefined || bestContrast < 20)
+   //   return DmtxFail;
 
    reg->sizeIdx = bestSizeIdx;
    reg->onColor = bestColorOnAvg;
