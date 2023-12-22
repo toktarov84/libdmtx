0405fbe3605a0b41d750fba46a628c7785139213
Vitaly Toktarov <70091281+toktarov84@users.noreply.github.com>
2023-12-22 14:57:37 +0400

    Update dmtxregion.c

--- dmtxregion.c
+++ dmtxregion.c
@@ -670,7 +670,7 @@ MatrixRegionFindSize(DmtxDecode *dec, DmtxRegion *reg)
    }
 
    /* Протестируйте каждый размер штрих-кода, чтобы найти наилучший контраст в калибровочных модулях */
-   for(sizeIdx = sizeIdxBeg; sizeIdx < sizeIdxEnd; sizeIdx++) {
+   for(sizeIdx = sizeIdxBeg; sizeIdx < 4; sizeIdx++) {
 
       symbolRows = dmtxGetSymbolAttribute(DmtxSymAttribSymbolRows, sizeIdx);
       symbolCols = dmtxGetSymbolAttribute(DmtxSymAttribSymbolCols, sizeIdx);
